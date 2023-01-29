10ebd850c23cc848ea026b92f5f35200e068bea0
madtro <anna.kozitzyna@yandex.ru>
2023-01-29 19:00:14 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,12 @@ public class Main {
         System.out.println("Данные ФИО сотрудника для заполнения отчета —" + fullName.toUpperCase());
     }
     public static void task3() {
-
+        String firstName = " Семён";
+        String middleName = " Семёнович";
+        String lastName = " Иванов";
+        String fullName = lastName + firstName + middleName;
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника —" + fullName);
     }
 
 }
