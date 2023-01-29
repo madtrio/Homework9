ded97047aee2b32882ea23c47d861e47276e4d7d
madtro <anna.kozitzyna@yandex.ru>
2023-01-29 18:53:11 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         task3();
     }
     public static void task1() {
+        String firstName = " Ivan";
+        String middleName = " Ivanovich";
+        String lastName = " Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника —" + fullName);
     }
     public static void task2() {
 
