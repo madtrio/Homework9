678997bafebabb77a68fec34f9d66e49ec393065
madtro <anna.kozitzyna@yandex.ru>
2023-01-29 18:46:44 +0300

    Starting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+    public static void task1() {
+    }
+    public static void task2() {
+
+    }
+    public static void task3() {
+
+    }
+
 }
