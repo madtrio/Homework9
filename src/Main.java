37bfda714c0c4095d83516e8df2bdb59414a1f5a
madtro <anna.kozitzyna@yandex.ru>
2023-02-05 18:04:01 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,39 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         task1();
         task2();
         task3();
     }
+
+    public static int determineYear(int year) {
+        if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("Год " + year + " является високосным");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println("Год " + year + " является високосным");
+        } else if (year % 100 == 0) {
+            System.out.println("Год " + year + " не является високосным");
+        } else {
+            System.out.println("Год " + year + " не является високосным");
+        }
+        return year;
+    }
+
+
     public static void task1() {
-        String firstName = " Ivan";
-        String middleName = " Ivanovich";
-        String lastName = " Ivanov";
-        String fullName = lastName + firstName + middleName;
-        System.out.println("ФИО сотрудника —" + fullName);
+        System.out.println("Task 1");
+        int year = 2200;
+        determineYear(year);
+
     }
+
+
     public static void task2() {
-        String firstName = " Ivan";
-        String middleName = " Ivanovich";
-        String lastName = " Ivanov";
-        String fullName = lastName + firstName + middleName;
-        System.out.println("Данные ФИО сотрудника для заполнения отчета —" + fullName.toUpperCase());
+
     }
+
     public static void task3() {
-        String firstName = " Семён";
-        String middleName = " Семёнович";
-        String lastName = " Иванов";
-        String fullName = lastName + firstName + middleName;
-        fullName = fullName.replace("ё", "е");
-        System.out.println("Данные ФИО сотрудника —" + fullName);
+
     }
 
 }
